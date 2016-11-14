37082a76f6db7bfaadf04fb19741186eeca19d2d
Craig1f <craig1f@yahoo.com>
2016-11-13 21:02:09 -0500

    Fixing ignorefile. Everything you need for Java spring scaffolding

--- src/main/webapp/resources/views/textarea/textarea.js
+++
@@ -1,8 +0,0 @@
-define(['angular'], function(angular) {
-	angular.module("navbar", []).controller('textareaController',
-			model);
-	model.$inject = ['$scope', '$log'];
-	function model($scope, $log, $window) {
-		$scope.description = 'default value';
-	}
-});
