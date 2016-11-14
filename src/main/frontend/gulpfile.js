b73e78a3c274a988f306f891190bf7fe73209f65
Craig1f <craig1f@yahoo.com>
2016-11-13 21:26:05 -0500

    got rid of webpack production config. Instead, modifies regular config for production build on the fly

--- src/main/frontend/gulpfile.js
+++ src/main/frontend/gulpfile.js
@@ -14,8 +14,17 @@ gulp.task('default', function(){
 		.pipe(gulp.dest('../webapp/'));
 });
 
-gulp.task('build:prod', function(){    
+gulp.task('build:prod', function(){   
+	var wp = require('webpack');
+	var config = require('./webpack.prod.config.js');
+	config.plugins.push(new wp.optimize.DedupePlugin())
+	config.plugins.push(new wp.optimize.UglifyJsPlugin({
+      compress: {warnings: false},
+      output: {comments: false},
+      sourceMap: true
+    }));
+    
     return gulp.src(['app/app.js'])
-        .pipe(webpack(require('./webpack.prod.config.js')))
+        .pipe(webpack(config))
         .pipe(gulp.dest('../webapp/'));
 });
